52c182063e5011f698e2fd13ac71316f652d0c7b
Greg Nietsky <gregory@distrotech.co.za>
2014-10-04 19:29:25 +0200

    Add dictionaries

---
+++ dictionaries/ru_RU/pythonpath/lightproof_opts_ru_RU.py
@@ -0,0 +1,4 @@
+lopts = {}
+lopts_default = {}
+lopts['ru_RU'] = [u'hyphen', u'comma', u'multiword', u'together', u'common', u'space', u'abbreviation', u'dup', u'numsep', u'typographica', u'quotation']
+lopts_default['ru_RU'] = [u'hyphen', u'comma', u'multiword', u'together', u'common', u'space', u'abbreviation', u'dup', u'typographica', u'quotation']
